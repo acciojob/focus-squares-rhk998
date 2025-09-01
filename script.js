498293e64275905f0a9238e6f388e0030eada2bc
rhk998 <130037816+rhk998@users.noreply.github.com>
2025-09-01 22:31:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+  square.addEventListener('mouseover', () => {
+    squares.forEach((s) => {
+      if (s !== square) {
+        s.style.backgroundColor = '#6F4E37'; // coffee
+      }
+    });
+  });
+
+  square.addEventListener('mouseout', () => {
+    squares.forEach((s) => {
+      s.style.backgroundColor = '#E6E6FA'; // lavender
+    });
+  });
+});
